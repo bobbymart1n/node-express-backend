fcc6d2170afcac1b838258a13b72fc721e46a8e7
Bobby Martin <robert.martin.619@gmail.com>
2019-04-14 23:08:05 -0700

    Add src directory and ignore dist, update package.json

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+const express = require( "express" );
+const app = express();
+const port = 4000; 
+
+// start the Express server
+app.listen( port, () => {
+    console.log( `server started at http://localhost:${ port }` );
+} );
