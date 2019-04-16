0ec8ea0696ccdd848d0112fd0ac1257c83254834
Bobby Martin <robert.martin.619@gmail.com>
2019-04-15 22:49:54 -0700

    Add typeorm and new file structure

--- src/server.ts
+++
@@ -1,8 +0,0 @@
-const express = require( "express" );
-const app = express();
-const port = 4000;
-
-// start the Express server
-app.listen( port, () => {
-    console.log( `server started at http://localhost:${ port }` );
-} );
