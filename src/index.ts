d8377f05ba2cf3d78e7ff9f59e79f143c725b6ff
Bobby Martin <robert.martin.619@gmail.com>
2019-04-22 22:33:10 -0700

    Add restructured express server and calls to get and create users

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,36 @@
+import express = require("express");
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 import { User } from "./entity/User";
 
 createConnection().then(async connection => {
-
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
-
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
-
-    const express = require("express");
     const app = express();
     const port = 4000;
+
+    app.get("/create", async (req, res) => {
+        const user = new User();
+        console.log("Inserting a new user into the database...");
+        user.firstName = "Timber";
+        user.lastName = "Saw";
+        user.age = 25;
+        await connection.manager.save(user);
+        console.log("Saved a new user with id: " + user.id);
+        res.send(user);
+    });
+
+    app.get("/read", async (req, res) => {
+        console.log("Loading users from the database...");
+        const users = await connection.manager.find(User);
+        console.log("Loaded users: ", users);
+        res.send(users);
+    });
+
+
+
+
+
+
+
 
     app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
